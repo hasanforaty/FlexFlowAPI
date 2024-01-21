c8c53f3d5db3821626d21581ca787e2e06357a4f
hasan foraty <hasanforaty19@gmail.com>
2024-01-21 19:44:02 +0330

    add a new user model with test

--- flex_flow_api/flex_flow_api/settings.py
+++ flex_flow_api/flex_flow_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_spectacular',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
+
+
+AUTH_USER_MODEL = 'core.User'
